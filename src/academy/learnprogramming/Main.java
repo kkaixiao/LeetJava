f6f774faf6882b288ae9bbaec5bef008716bc768
kkaixiao <show-key@qq.com>
2020-08-15 05:36:11 -0600

    first

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("test, let's test, not hello world");
+    }
+}
